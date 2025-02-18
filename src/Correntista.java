14da9cd27b1f9d11a5e933fd43dc0f0c2e939107
João Pedro Souza Arruda <125840345+Jparruda@users.noreply.github.com>
2025-02-18 13:02:08 -0300

    Create Correntista.java

---
+++ src/Correntista.java
@@ -0,0 +1,21 @@
+public class Correntista {
+private String opfCliente; // cpt do coccentista
+private float saldo;// saldo da conta
+
+public Correntista(String cpfCliente, float saldo) {
+this.cpfCliente cpfClienter;
+this.saldo = saldo;
+}
+
+public String getCPECliente(){
+return cpfCliente;
+}
+
+public float getSaldo() {
+return saldo
+}
+
+public void setSaldo (float saldo) {
+this.saldo saldo
+}
+}
